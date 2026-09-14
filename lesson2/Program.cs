ed0bb749eb061f9a8961e342388b106f4253aa35
user <user@WIN-9E33VFDFIJO>
2026-09-14 08:18:05 +0500

    Добавьте файлы проекта.

---
+++ lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> EnemyHp = new Dictionary<string, int>();
+            EnemyHp.Add("Zombie", 100);
+            EnemyHp.Add("Boss", 200);
+            EnemyHp.Add("Player", 50);
+            EnemyHp["Boss"] = 350;
+            Console.WriteLine(EnemyHp["Boss"]);
+            
+        }
+
+           
+        
+    }
+}
