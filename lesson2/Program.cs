3f2fc1270643a50d617d02c0d157cfa9684a06ab
DeVlog <devlog790@gmail.com>
2026-09-14 08:25:50 +0500

    add!

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -8,11 +8,11 @@ namespace lesson2
         static void Main(string[] args)
         {
             Dictionary<string, int> EnemyHp = new Dictionary<string, int>();
-            EnemyHp.Add("Zombie", 100);
-            EnemyHp.Add("Boss", 200);
-            EnemyHp.Add("Player", 50);
-            EnemyHp["Boss"] = 350;
-            Console.WriteLine(EnemyHp["Boss"]);
+            EnemyHp.Add("Zombie!", 100);
+            EnemyHp.Add("Boss!", 200);
+            EnemyHp.Add("Player!", 50);
+            EnemyHp["Boss!"] = 350;
+            Console.WriteLine(EnemyHp["Boss!"]);
             
         }
 
